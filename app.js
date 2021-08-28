3e4680379a124efe7218876cd92e8a8f66092e94
ErjillDeVera <erjilldevera@icloud.com>
2021-08-28 09:10:23 +0100

    Implemented adding posts to the home page.

--- app.js
+++ app.js
@@ -18,10 +18,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-  for(var i = 0; i < posts.length; i++) {
-    console.log(posts[i]);
-  }
-  res.render("home", {homeStartingContent: homeStartingContent});
+  res.render("home", {homeStartingContent: homeStartingContent, posts: posts});
 });
 
 app.get("/about", function(req, res) {
