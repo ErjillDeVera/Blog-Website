be85fe56166c2f50cb1e5f43fe02188e201c16ef
ErjillDeVera <erjilldevera@icloud.com>
2021-09-16 11:43:23 +0100

    Connected app to local database.

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+
 app.get("/", function(req, res){
   res.render("home", {homeStartingContent: homeStartingContent, posts: posts});
 });
